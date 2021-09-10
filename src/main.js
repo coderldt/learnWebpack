d54d9fb9dde8c2b649ed1f100f183e1d348e8ab5
lvchao <1969956387@qq.com>
2021-09-10 18:27:22 +0800

    feat: 资源管理

--- src/main.js
+++ src/main.js
@@ -1,9 +1,41 @@
 import _ from 'lodash'
+
+import './main.css'
+
+import img from './back.png'
+
+import DataXml from './data.xml'
+import DataCsv from './data.csv'
+
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
 function component() {
     const ele = document.createElement('div')
     ele.innerHTML = _.join(['hello', 'word'], ' ')
 
+    ele.classList.add('hi')
+
+    const pic = document.createElement('img')
+    pic.src = img
+    ele.appendChild(pic)
+
     return ele
 }
+
+console.log(DataXml, DataCsv);
+
+
 
 document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,53 @@ const path = require('path')
 module.exports = {
     entry: "./src/main.js",
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: '2048'
+                    }
+                }
+            },
+            {
+                test: /\.xml/,
+                use: 'xml-loader'
+            },
+            {
+                test: /\.csv/,
+                use: 'csv-loader'
+            },
+            {
+                test: /\.toml$/,
+                type: 'json',
+                parser: {
+                    parse: require('toml').parse
+                }
+            },
+            {
+                test: /\.yaml$/,
+                type: 'json',
+                parser: {
+                    parse: require(yamljs).parse
+                }
+            },
+            {
+                test: /\.json5$/,
+                type: 'json',
+                parser: {
+                    parse: 'json5.parse'
+                }
+            },
+        ]
     }
 }
